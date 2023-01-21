07d5eedb4b4559f8a92d7cdeb63cf6f57c698238
auditiochowdhury <auditioc@gmail.com>
2023-01-21 13:24:44 -0500

    So far I have the 16x16 grids, trouble targeting them

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
 console.log('Hello')
+
+//Create 16x16 grid of square divs, First we make the grid element and give it a class of theGrids
+
+
+//Grab the container div and store it in a variable
+const parentDiv = document.querySelector('.container')
+
+// Append the grid element we made to the container Div in our html file
+function CreateGrid(){
+    for(let i= 0;i<273;i+=1){
+        let grid= document.createElement('div')
+        grid.classList.add("theGrids");
+        parentDiv.appendChild(grid)
+    }
+}
+//Calling the grid
+CreateGrid()
+
+
+//For each time you hover on one of the square grids, we want to change the color the color of the div, so on hover we add class i think
+document.querySelector('.theGrids').onclick = changeColor
+
+//Function that changes the color of the div
+function changeColor(){
+    console.log('Click!')
+    alert('clicked!')
+    // document.querySelector('.theGrids').style.backgroundColor = yellow
+}
